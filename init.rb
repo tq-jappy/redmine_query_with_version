2f71e13e1fe6ae2aeb48a7c2295d6ec32fa8c427
Takumi Endo <tq-ed@v7.com>
2014-06-12 00:58:22 +0900

    update init.rb

---
+++ init.rb
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+Redmine::Plugin.register :redmine_query_with_version do
+  name 'Redmine Query with Version plugin'
+  author 'Author name'
+  description 'Plugin to search issues by a custom query and any version.'
+  version '0.0.1'
+  url 'https://github.com/tq-jappy/redmine_query_with_version'
+  author_url 'https://github.com/tq-jappy/redmine_query_with_version'
+
+  requires_redmine :version_or_higher => '2.3'
+
+  project_module :query_with_version do
+    # permission :select_version, :query_with_version => [:select_version]
+  end
+end
+
+require 'selectable_query'
