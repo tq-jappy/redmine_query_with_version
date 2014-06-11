20f85693d3cdbe539951b5f7e7fe275c098deb68
Takumi Endo <tq-ed@v7.com>
2014-06-12 01:52:20 +0900

    update author name.

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@
 
 Redmine::Plugin.register :redmine_query_with_version do
   name 'Redmine Query with Version plugin'
-  author 'Author name'
+  author 'Takumi Endo'
   description 'Plugin to search issues by a custom query and any version.'
   version '0.0.1'
   url 'https://github.com/tq-jappy/redmine_query_with_version'
