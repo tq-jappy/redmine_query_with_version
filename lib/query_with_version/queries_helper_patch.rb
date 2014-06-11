7d3ee9094d7cffd0bfbf545b73fb76bdc30c125f
Takumi Endo <tq-ed@v7.com>
2014-06-12 02:13:23 +0900

    refactor. extract complex conditions

--- lib/query_with_version/queries_helper_patch.rb
+++ lib/query_with_version/queries_helper_patch.rb
@@ -16,9 +16,18 @@ module QueryWithVersion
     module InstanceMethods
       def retrieve_query_with_version
         retrieve_query_without_version
+
         if params[:query_id].present? and params[:fixed_version_id].present?
+          session[:query][:fixed_version_id] = params[:fixed_version_id]
           @query.add_filter("fixed_version_id", "=", [params[:fixed_version_id]])
+        elsif !query_built_from_params? && session[:query][:fixed_version_id]
+          @query.add_filter("fixed_version_id", "=", [session[:query][:fixed_version_id]])
         end
+      end
+     
+      def query_built_from_params?
+        api_request? || params[:set_filter] || session[:query].nil? || 
+          session[:query][:project_id] != (@project ? @project.id : nil)
       end
     end
   end
