9842c6a5ea7576f08273b2dbf6170cc53706133d
Takumi Endo <tq-ed@v7.com>
2014-08-26 01:20:49 +0900

    refactor.

--- lib/query_with_version.rb
+++ lib/query_with_version.rb
@@ -1,9 +1,7 @@
 require 'query_with_version/hooks'
 
+require 'query_with_version/queries_helper_patch'
+
 Rails.configuration.to_prepare do
   require 'query_with_version/queries_helper_patch'
-  require 'query_with_version/issues_controller_patch'
-end
-
-module QueryWithVersion
 end
